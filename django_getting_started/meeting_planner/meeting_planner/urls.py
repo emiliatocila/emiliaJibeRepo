77cf9d5be08ba9b1ef2a4a1f6a583a9c2575d127
emiliatocila <emilia.tocila@jibecompany.com>
2020-07-24 16:03:33 +0300

    Django - urls and link building

--- django_getting_started/meeting_planner/meeting_planner/urls.py
+++ django_getting_started/meeting_planner/meeting_planner/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from website.views import welcome
+from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome)
+    path('', welcome, name="welcome"),
+    path('meetings/<int:id>', detail, name="detail"),
 ]
--- django_getting_started/meeting_planner/website/views.py
+++ django_getting_started/meeting_planner/website/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from meetings.models import Meeting
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
