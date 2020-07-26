16077fb0d8da7d1bf52c238865f88df2d948c156
emiliatocila <emilia.tocila@jibecompany.com>
2020-07-26 19:00:23 +0300

    Django REST - part1 - Serialization

---
+++ django_serialization/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jibe/Documents/practiceRepo/emiliaJibeRepo/django_serialization/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
