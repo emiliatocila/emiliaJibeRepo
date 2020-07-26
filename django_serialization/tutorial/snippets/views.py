b4767d4d15472759cfc4f030da21417d635eec83
emiliatocila <emilia.tocila@jibecompany.com>
2020-07-26 19:44:45 +0300

    Django REST - part5 - Relationships and hyperlinked APIs

--- django_serialization/tutorial/snippets/views.py
+++ django_serialization/tutorial/snippets/views.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -30,3 +36,19 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)})
